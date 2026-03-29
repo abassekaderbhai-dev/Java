d3f934ffec473284705a7810a17cd62e33964488
abassekaderbhai-dev <abassekaderbhai@gmail.com>
2026-03-29 20:38:17 +0200

    Add files via upload

---
+++ CompteBancaire.java
@@ -0,0 +1,45 @@
+/** correction de CompteBancaire suite aux premiers Tps
+*/
+
+
+public class CompteBancaire {
+
+
+   /**
+   * Constructeurs
+   */
+
+
+
+    // getters setters
+
+
+   /**
+   * ajoute 'ajout 'au solde si 'ajout' est positif et retourne true, sinon retourne false
+   */
+
+
+
+    /**
+   * retire 'retrait' au solde si 'retrait' est positif et retourne true, retourne false si 'retrait' est négatif
+   */
+
+   /**
+   * test si deux comptes ont memes proprietaires
+
+   * transfert d'un compte sur un autre pour un meme proprietaire
+   * le compte passé en parametre est receveur du transfert
+   * la valeur est donc positive
+   * renvoie faux si erreur
+   * vrai si le tranfert est effectué
+   * fait des affichages appropriés dans le cas d'un retour à false (3 cas)
+   */
+
+
+
+   /*
+   * retourne la conversion du solde du compte dans une monnaie dont le taux de change est passé en paramètre
+   */
+
+
+}
---
+++ TestCompteBancaire.java
@@ -0,0 +1,10 @@
+/******************** Test de la classe *****************************/
+
+public class TestCompteBancaire
+{
+
+public static void main(String args[]) {
+
+}// fin du main
+} // fin de la classe
+
