1b9c892c7f67e8e31436b31f35bab73be9100877
abassekaderbhai-dev <abassekaderbhai@gmail.Com>
2026-04-05 13:13:18 +0200

    premier commit

---
+++ CompteBancaire/TestCompteBancaire.java
@@ -0,0 +1,20 @@
+public class TestCompteBancaire
+{
+
+public static void main(String args[]) {
+    
+    CompteBancaire Abasse = new CompteBancaire("Abasse", "1234567FR", 382.45f);
+    CompteBancaire Jawed = new CompteBancaire("Jawed", "987654FR", 0f);
+    Abasse.afficher();
+    float solde1 = Abasse.getSolde();
+    boolean egalite = Abasse.memeProprio(Jawed);
+    System.out.println("Je suis riche de : " + solde1) ;
+    System.out.println("Est-ce que c'est les mêmes personnes : " + egalite) ;
+    boolean solde2 = Abasse.retirer(50);
+    boolean solde3 = Abasse.retirer(400);
+    boolean solde4 = Jawed.ajouter(60);
+    System.out.println("1er retrait Abasse: " + solde2 + " solde : " + Abasse.getSolde() + " 2nd retrait Abasse : " + solde3 + " solde : " + Abasse.getSolde() + 
+    " 1er depot Jawed : " + solde4 + " solde : " + Jawed.getSolde());
+}
+} 
+
---
+++ README.md
@@ -0,0 +1,16 @@
+# Java
+
+Bienvenue dans mon dépôt GitHub dédié aux projets en Java.  
+Ici, je centralise tous mes projets académiques et personnels réalisés dans le cadre de mon BUT Informatique.
+
+## Contenu
+
+Chaque dossier correspond à un projet Java.  
+Vous y trouverez les fichiers source, programmes et éventuellement des ressources associées.
+
+## Utilisation
+
+Pour utiliser un projet :
+
+```bash
+git clone https://github.com/abassekaderbhai-dev/Java.git
